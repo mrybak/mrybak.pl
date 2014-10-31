fee648c7cf966d6944b175b170abbae661b5bd2b
Michał Rybak <kontakt@mrybak.pl>
2014-10-31 03:09:44 +0100

    improved navbar behaviour

--- js/main.js
+++ js/main.js
@@ -4,23 +4,24 @@ $(function () {
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function () {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    var $navbar = $(".navbar-fixed-top");
+    var $home_link = $("#home-link");
+    var $is_scrolled = $(".navbar").offset().top > 50;
+    if ($is_scrolled) {
+        $navbar.addClass("top-nav-collapse");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $navbar.removeClass("top-nav-collapse");
     }
+    $home_link.toggleClass("hidden", !$is_scrolled);
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function () {
     $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
-        var $home_link = $("#home-link");
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo', function () {
-            $home_link.toggleClass("hidden", $anchor.hasClass("home-scroll"));
-        });
+        }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
