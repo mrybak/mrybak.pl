1b138a101c2d7edd84eb84336ab8e9f158173bbe
Michał Rybak <kontakt@mrybak.pl>
2014-10-31 02:46:09 +0100

    basic scrolling features; big mess in code & content for a while

--- js/main.js
+++ js/main.js
@@ -2,3 +2,25 @@ $(function () {
     $("[data-toggle=tooltip]").tooltip();
 });
 
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function () {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        var $home_link = $("#home-link");
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo', function () {
+            $home_link.toggleClass("hidden", $anchor.hasClass("home-scroll"));
+        });
+        event.preventDefault();
+    });
+});
